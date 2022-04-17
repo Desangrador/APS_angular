76474058291400bc141539c87c9a1936ee4b7be5
Desangrador <017100864j@uandina.edu.pe>
2022-04-17 15:21:19 -0500

    Productos-Routing2

--- src/app/paginas/lotes/lotes.component.ts
+++ src/app/paginas/lotes/lotes.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Lote, Lotes } from 'src/app/Interfaces';
 
 @Component({
   selector: 'app-lotes',
   templateUrl: './lotes.component.html',
-  styles: [
-  ]
+  styleUrls: [ '../../app.component.css' ]
 })
 export class LotesComponent implements OnInit {
-
   constructor() { }
-
   ngOnInit(): void {
   }
 
+  listarLotes(): Lote[]{
+    return Lotes
+  }
+
+  
 }
